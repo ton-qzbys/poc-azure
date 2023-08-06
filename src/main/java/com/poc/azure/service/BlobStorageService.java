cea3c3cf3028532ae24ce178252bb9a952255098
Bunyasan K <tonasds007@hotmail.com>
2023-08-06 23:57:50 +0700

    Revert "save"
    This reverts commit 96a1382c03cce7f87f638829bcc90c014b400065.

--- src/main/java/com/poc/azure/service/BlobStorageService.java
+++
@@ -1,9 +0,0 @@
-package com.poc.azure.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.net.URI;
-
-public interface BlobStorageService {
-    public URI uploadPicture(MultipartFile multipartFile);
-}
